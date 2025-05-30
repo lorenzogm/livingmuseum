a4a5d86573dcda1a2ba2cb380b28a7ec4d566889
Lorenzo García Moreno <lorenzo.garciamoreno@valtech.com>
2025-05-30 23:06:45 +0200

    Reorganize artist data imports and update allArtists array for alphabetical sorting

--- src/api/artists.ts
+++ src/api/artists.ts
@@ -1,19 +1,19 @@
 // Artists content and types
 import artistsPageContent from '../../public/pages/artists.es.json';
 
-// Import individual artist data
-import zarcoData from '../../public/content/artistas/zarco/zarco.json';
-import gabrielData from '../../public/content/artistas/gabriel-pastor-guzman/gabriel-pastor-guzman.json';
+// Import individual artist data (alphabetically sorted)
 import blancaData from '../../public/content/artistas/blanca-valcarce-quiroga/blanca-valcarce-quiroga.json';
-import fernandoData from '../../public/content/artistas/fernando-jose-escrina/fernando-jose-escrina.json';
-import miguelData from '../../public/content/artistas/miguel-angel-ruiz/miguel-angel-ruiz.json';
-import gustavoData from '../../public/content/artistas/gustavo-pannullo-gap/gustavo-pannullo-gap.json';
 import carlosData from '../../public/content/artistas/carlos-molina-vallejo/carlos-molina-vallejo.json';
-import manuelData from '../../public/content/artistas/manuel-vela/manuel-vela.json';
-import joseData from '../../public/content/artistas/jose-manuel-lopez/jose-manuel-lopez.json';
+import fernandoData from '../../public/content/artistas/fernando-jose-escrina/fernando-jose-escrina.json';
+import gabrielData from '../../public/content/artistas/gabriel-pastor-guzman/gabriel-pastor-guzman.json';
+import gustavoData from '../../public/content/artistas/gustavo-pannullo-gap/gustavo-pannullo-gap.json';
 import iruneData from '../../public/content/artistas/irune-tanco/irune-tanco.json';
+import joseData from '../../public/content/artistas/jose-manuel-lopez/jose-manuel-lopez.json';
 import malenaData from '../../public/content/artistas/malena-hidalgo/malena-hidalgo.json';
+import manuelData from '../../public/content/artistas/manuel-vela/manuel-vela.json';
+import miguelData from '../../public/content/artistas/miguel-angel-ruiz/miguel-angel-ruiz.json';
 import pilarData from '../../public/content/artistas/pilar-sanchez-baidez/pilar-sanchez-baidez.json';
+import zarcoData from '../../public/content/artistas/zarco/zarco.json';
 
 // Artist interfaces
 export interface Artwork {
@@ -50,20 +50,20 @@ export interface ArtistsPageContent {
   artists: ArtistProfile[];
 }
 
-// Combine all artist data
+// Combine all artist data (alphabetically sorted by name)
 const allArtists: ArtistProfile[] = [
-  zarcoData,
-  gabrielData,
   blancaData,
-  fernandoData,
-  miguelData,
-  gustavoData,
   carlosData,
-  manuelData,
-  joseData,
+  fernandoData,
+  gabrielData,
+  gustavoData,
   iruneData,
+  joseData,
   malenaData,
-  pilarData
+  manuelData,
+  miguelData,
+  pilarData,
+  zarcoData
 ] as ArtistProfile[];
 
 // Artists content loader
